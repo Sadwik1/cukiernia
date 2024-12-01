678f9e291a064581b6903d8366270b51e8fb134c
Sadwik <114020422+Sadwik1@users.noreply.github.com>
2024-12-01 23:32:39 +0100

    Add files via upload
    baza

--- cukiernia.sql
+++ cukiernia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 26, 2024 at 03:01 PM
--- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.2.12
+-- Czas generowania: 01 Gru 2024, 23:31
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cukiernia`
+-- Baza danych: `cukiernia`
 --
 
 -- --------------------------------------------------------
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `kategorieproduktow` (
   `idKP` int(11) NOT NULL,
   `nazwa` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kategorieproduktow`
+-- Zrzut danych tabeli `kategorieproduktow`
 --
 
 INSERT INTO `kategorieproduktow` (`idKP`, `nazwa`) VALUES
@@ -52,18 +52,18 @@ CREATE TABLE `klienci` (
   `imie` varchar(50) NOT NULL,
   `nazwisko` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `telefon` varchar(13) NOT NULL,
   `haslo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `klienci`
+-- Zrzut danych tabeli `klienci`
 --
 
-INSERT INTO `klienci` (`idK`, `imie`, `nazwisko`, `email`, `telefon`, `haslo`) VALUES
-(1, 'Jan', 'Kowalski', 'jan.kowalski@example.com', '123456789', ''),
-(2, 'Anna', 'Nowak', 'anna.nowak@example.com', '987654321', ''),
-(3, 'Piotr', 'Wiśniewski', 'piotr.wisniewski@example.com', '555123456', '');
+INSERT INTO `klienci` (`idK`, `imie`, `nazwisko`, `email`, `haslo`) VALUES
+(1, 'Jan', 'Kowalski', 'jan.kowalski@example.com', 'bzyczek123'),
+(2, 'Anna', 'Nowak', 'anna.nowak@example.com', 'wryczek234'),
+(3, 'Piotr', 'Wiśniewski', 'piotr.wisniewski@example.com', 'kopal965'),
+(4, 'Wiktor', 'Sadłowski', 'sadwik2054@gmail.com', 'Sadwik2054');
 
 -- --------------------------------------------------------
 
@@ -79,17 +79,17 @@ CREATE TABLE `produkty` (
   `wartosc_odzywcza` text DEFAULT NULL,
   `zdjecie` varchar(50) DEFAULT NULL,
   `idKP` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `produkty`
+-- Zrzut danych tabeli `produkty`
 --
 
 INSERT INTO `produkty` (`idP`, `nazwa`, `cena`, `skladniki`, `wartosc_odzywcza`, `zdjecie`, `idKP`) VALUES
-(1, 'Sernik', 15.99, 'Ser, biszkopty, śmietana', 'Wysoka zawartość tłuszczu', 'sernik.jpg', 1),
-(2, 'Ciasto czekoladowe', 12.99, 'Czekolada, mąka, jajka', 'Średnia zawartość tłuszczu', 'ciasto_czekoladowe.jpg', 1),
-(3, 'Pierniczki', 8.99, 'Mąka, miód, przyprawy', 'Niska zawartość tłuszczu', 'pierniczki.jpg', 2),
-(4, 'Tort truskawkowy', 25.99, 'Biszkopt, krem, truskawki', 'Wysoka zawartość cukru', 'tort_truskawkowy.jpg', 3);
+(1, 'Sernik', '15.99', 'Ser, biszkopty, śmietana', 'Wysoka zawartość tłuszczu', 'sernik.jpg', 1),
+(2, 'Ciasto czekoladowe', '12.99', 'Czekolada, mąka, jajka', 'Średnia zawartość tłuszczu', 'ciasto_czekoladowe.jpg', 1),
+(3, 'Pierniczki', '8.99', 'Mąka, miód, przyprawy', 'Niska zawartość tłuszczu', 'pierniczki.jpg', 2),
+(4, 'Tort truskawkowy', '25.99', 'Biszkopt, krem, truskawki', 'Wysoka zawartość cukru', 'tort_truskawkowy.jpg', 3);
 
 -- --------------------------------------------------------
 
@@ -101,17 +101,18 @@ CREATE TABLE `zamowienia` (
   `idZ` int(11) NOT NULL,
   `idP` int(11) NOT NULL,
   `idK` int(11) NOT NULL,
-  `ilosc` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ilosc` int(15) NOT NULL,
+  `data_wykonania` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `zamowienia`
+-- Zrzut danych tabeli `zamowienia`
 --
 
-INSERT INTO `zamowienia` (`idZ`, `idP`, `idK`, `ilosc`) VALUES
-(1, 1, 1, 2),
-(2, 3, 2, 1),
-(3, 4, 3, 1);
+INSERT INTO `zamowienia` (`idZ`, `idP`, `idK`, `ilosc`, `data_wykonania`) VALUES
+(1, 1, 1, 2, '2023-12-14'),
+(2, 3, 2, 1, '2023-12-18'),
+(3, 4, 3, 1, '2024-01-22');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -145,45 +146,45 @@ ALTER TABLE `zamowienia`
   ADD KEY `idK` (`idK`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
--- AUTO_INCREMENT for table `kategorieproduktow`
+-- AUTO_INCREMENT dla tabeli `kategorieproduktow`
 --
 ALTER TABLE `kategorieproduktow`
   MODIFY `idKP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `klienci`
+-- AUTO_INCREMENT dla tabeli `klienci`
 --
 ALTER TABLE `klienci`
-  MODIFY `idK` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idK` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `produkty`
+-- AUTO_INCREMENT dla tabeli `produkty`
 --
 ALTER TABLE `produkty`
   MODIFY `idP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `zamowienia`
+-- AUTO_INCREMENT dla tabeli `zamowienia`
 --
 ALTER TABLE `zamowienia`
   MODIFY `idZ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `produkty`
+-- Ograniczenia dla tabeli `produkty`
 --
 ALTER TABLE `produkty`
   ADD CONSTRAINT `produkty_ibfk_1` FOREIGN KEY (`idKP`) REFERENCES `kategorieproduktow` (`idKP`);
 
 --
--- Constraints for table `zamowienia`
+-- Ograniczenia dla tabeli `zamowienia`
 --
 ALTER TABLE `zamowienia`
   ADD CONSTRAINT `zamowienia_ibfk_1` FOREIGN KEY (`idP`) REFERENCES `produkty` (`idP`),
